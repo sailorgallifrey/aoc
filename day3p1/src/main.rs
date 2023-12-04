b34230ac1c9467852e014ae4cd45b9ff615a1d86
JWATKDK <bryantjohnnyc@johndeere.com>
2023-12-04 12:41:54 -0600

    Day 3

---
+++ day3p1/src/main.rs
@@ -0,0 +1,96 @@
+use std::fs;
+use std::slice::Iter;
+
+fn main() {
+    
+
+    let data = fs::read_to_string("./data.txt")
+        .expect("Couldn't read file.");
+
+    let map: Vec<Vec<char>> = get_map(data);
+    let mut total: u32 = 0;
+
+    for i in 0..map.len() {
+        let mut j: usize = 0;
+        while j < map[0].len() {
+            let starting_char: char = map[i][j];
+
+            if starting_char.is_numeric() {
+                let (include, num_str) = check_for_number(&map, i, &mut j, starting_char);
+
+                if include {
+                    total += num_str.parse::<u32>().unwrap();
+                }
+                j = j + num_str.len() ;
+
+            } else {
+                j += 1;
+            }
+        }
+    }
+
+    println!("{:?}", total);
+}
+
+fn check_for_number(map: &Vec<Vec<char>>, i: usize, j: &mut usize, starting_char: char) -> (bool, String) {
+    let mut current_char: char = starting_char;
+    let mut include: bool = false;
+    let mut j2: usize = *j;
+    let mut num_str: String = String::from("");
+
+    while j2 < map[0].len() {
+        if j2 != *j {
+            current_char = map[i][j2];
+        }
+        if current_char.is_numeric() {
+            num_str.push(current_char);
+
+            for (k, l) in get_queens_case(i, j2, map[0].len(), map.len()) {
+                if !map[k][l].is_numeric() && !map[k][l].eq(&'.') {
+                    include = true;
+                    break;
+                }
+            }
+        } else if !num_str.is_empty() {
+            break;
+        }
+        j2 += 1;
+    }
+    (include, num_str)
+}
+
+fn get_map(data: String) -> Vec<Vec<char>> {
+    data.lines()
+        .map(|l| l.chars().collect())
+        .collect()
+}
+
+fn get_queens_case(i: usize, j: usize, w: usize, h: usize) -> Vec<(usize, usize)> {
+    let mut window: Vec<(usize, usize)> = vec![];
+
+    if i > 0 {
+        if j > 0 {
+            window.push((i - 1, j - 1));
+        }
+        window.push((i - 1, j));
+        if j < w - 1{
+            window.push((i - 1, j + 1))
+        }
+    }
+    if j > 0 {
+        window.push((i, j - 1));
+    }
+    if j < w - 1 {
+        window.push((i, j + 1));
+    }
+    if i < h - 1 && j > 0 {
+        window.push((i + 1, j - 1));
+    }
+    if i < h - 1 {
+        window.push((i + 1, j));
+    }
+    if i < h - 1 && j < w - 1 {
+        window.push((i + 1, j + 1));
+    }
+    window
+}
---
+++ day3p2/src/main.rs
@@ -0,0 +1,109 @@
+use std::fs;
+use std::slice::Iter;
+
+fn main() {
+    
+
+    let data = fs::read_to_string("./data.txt")
+        .expect("Couldn't read file.");
+
+    let map: Vec<Vec<char>> = get_map(data);
+    let mut total: u32 = 0;
+
+    for i in 0..map.len() {
+        let mut j: usize = 0;
+        while j < map[0].len() {
+            let result: (bool, u32) = check_for_ratio(&map, i, j, map[i][j]);
+
+            if result.0 {
+                total += result.1;
+            }
+            j += 1 ;
+        }
+    }
+
+    println!("{:?}", total);
+}
+
+fn check_for_ratio(map: &Vec<Vec<char>>, i: usize, j: usize, current_char: char) -> (bool, u32) {
+    if current_char.eq(&'*') {
+        let nums: Vec<u32> = find_surrounding_numbers(i, j, map);
+
+        if nums.len() == 2 {
+            return (true, nums[0] * nums[1])
+        } else { return (false, 0) }
+    }
+
+    (false, 0)
+}
+
+fn find_surrounding_numbers(i: usize, j: usize, map: &Vec<Vec<char>>) -> Vec<u32> {
+    let qc = get_queens_case_plus_current(i, j, map[0].len(), map.len());
+    let mut nums: Vec<u32> = vec![];
+
+    let mut used_locations: Vec<(usize, usize)> = vec![];
+    for (k, l) in qc {
+        let mut num_str = String::from("");
+        if map[k][l].is_numeric() && !used_locations.contains(&(k, l)) {
+            let mut l1 = l;
+            loop { // find number's left boundary
+                if l1 > 0 && map[k][l1 - 1].is_numeric() {
+                    l1 -= 1;
+                    continue;
+                } else {
+                    used_locations.push((k,l1));
+                    num_str.push(map[k][l1]);
+                    break
+                }
+            }
+            loop { // move right
+                l1 += 1;
+                if l1 < map[k].len() && map[k][l1].is_numeric() {
+                    used_locations.push((k, l1));
+                    num_str.push(map[k][l1]);
+                } else { break }
+            }
+
+            nums.push(num_str.parse::<u32>().expect("Unable to parse number"))
+        }
+    }
+
+    nums
+}
+
+fn get_map(data: String) -> Vec<Vec<char>> {
+    data.lines()
+        .map(|l| l.chars().collect())
+        .collect()
+}
+
+fn get_queens_case_plus_current(i: usize, j: usize, w: usize, h: usize) -> Vec<(usize, usize)> {
+    let mut window: Vec<(usize, usize)> = vec![];
+
+    window.push((i, j));
+    if i > 0 {
+        if j > 0 {
+            window.push((i - 1, j - 1));
+        }
+        window.push((i - 1, j));
+        if j < w - 1{
+            window.push((i - 1, j + 1))
+        }
+    }
+    if j > 0 {
+        window.push((i, j - 1));
+    }
+    if j < w - 1 {
+        window.push((i, j + 1));
+    }
+    if i < h - 1 && j > 0 {
+        window.push((i + 1, j - 1));
+    }
+    if i < h - 1 {
+        window.push((i + 1, j));
+    }
+    if i < h - 1 && j < w - 1 {
+        window.push((i + 1, j + 1));
+    }
+    window
+}
