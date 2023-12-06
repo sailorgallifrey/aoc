d2afa364885a3f7d3799c932e39cfcc670e8d859
JWATKDK <bryantjohnnyc@johndeere.com>
2023-12-05 20:38:26 -0600

    Day 5

---
+++ day5p1/src/main.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("./data.txt").expect("Couldn't read file.");
+    let lines: Vec<&str> = file.lines().collect();
+
+    let seeds: Vec<u64> = get_seeds(lines.first().unwrap());
+    let maps: HashMap<String, Vec<Vec<u64>>> = get_maps(lines);
+
+    let result: u64 = get_result(seeds, maps);
+
+    println!("{:?}", result);
+}
+
+fn get_seeds(s: &str) -> Vec<u64> {
+    s.split(": ")
+        .last()
+        .unwrap()
+        .split(' ')
+        .map(|n| n.parse::<u64>().expect("Unable to parse seed id"))
+        .collect()
+}
+
+fn get_maps(lines: Vec<&str>) -> HashMap<String, Vec<Vec<u64>>> {
+    let mut result: HashMap<String, Vec<Vec<u64>>> = HashMap::new();
+    let mut map_name: String = String::from("");
+    let mut map_vec: Vec<Vec<u64>> = vec![];
+    for line in lines.iter().skip(1) {
+        if line.contains("map:") {
+            map_name = line.replace(" map:", "");
+        } else if !map_name.is_empty() && line.is_empty() {
+            result.insert(map_name.clone(), map_vec.clone());
+            map_vec = vec![];
+        }
+        else if !line.trim().is_empty() {
+            let nums: Vec<u64> = line
+                .split(' ')
+                .map(|n| n.parse::<u64>().expect("failed to parse map number"))
+                .collect();
+
+            map_vec.push(nums)
+        }
+    }
+
+    result.insert(map_name.clone(), map_vec.clone());
+    result
+}
+
+fn get_result(seeds: Vec<u64>, map: HashMap<String, Vec<Vec<u64>>>) -> u64 {
+    let mut lowest_location: u64 = u64::MAX;
+    let seed_map = &map["seed-to-soil"];
+    let soil_map = &map["soil-to-fertilizer"];
+    let fert_map = &map["fertilizer-to-water"];
+    let water_map = &map["water-to-light"];
+    let light_map = &map["light-to-temperature"];
+    let temp_map = &map["temperature-to-humidity"];
+    let humid_map = &map["humidity-to-location"];
+
+    for seed in seeds {
+        let soil: u64 = get_value(&seed, seed_map);
+        let fertilizer: u64 = get_value(&soil, soil_map);
+        let water: u64 = get_value(&fertilizer, fert_map);
+        let light: u64 = get_value(&water, water_map);
+        let temp: u64 = get_value(&light, light_map);
+        let humid: u64 = get_value(&temp, temp_map);
+        let location: u64 = get_value(&humid, humid_map);
+
+        if lowest_location > location {
+            lowest_location = location.to_owned()
+        }
+    }
+
+    lowest_location
+}
+
+fn get_value(lookup: &u64, map: &Vec<Vec<u64>>) -> u64 {
+    for m in map {
+        let last = m[1] + m[2];
+        if *lookup >= m[1] && *lookup <= last {
+            return m[0] + (*lookup - m[1])
+        }
+    }
+    lookup.to_owned()
+}
