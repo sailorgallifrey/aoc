2dcdc136bcd135b5b8b0b5967cf57119014cbf7f
JWATKDK <bryantjohnnyc@johndeere.com>
2023-12-08 15:36:11 -0600

    Day 8 p1

--- day8p1/src/main.rs
+++ day8p1/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs;
-use std::ops::Add;
 
 #[derive(Hash, Eq, PartialEq, Clone)]
 enum Direction {
@@ -19,28 +18,17 @@ impl From<char> for Direction {
 }
 
 #[derive(Clone, Eq, PartialEq)]
-struct Address {
-    chars: [char; 3],
-}
-
-impl From<[char; 3]> for Address {
-    fn from(value: [char; 3]) -> Self {
-        Address { chars: value }
-    }
-}
-
-#[derive(Clone, Eq, PartialEq)]
-struct Map {
-    address: Address,
-    destinations: HashMap<Direction, Address>,
+struct Location {
+    address: Box<str>,
+    destinations: HashMap<Direction, Box<str>>,
 }
 
 struct Problem {
     directions: Vec<Direction>,
-    maps: Vec<Map>,
+    locations: Vec<Location>,
 }
 fn main() {
-    let file = fs::read_to_string("./data-test.txt").expect("Couldn't read file.");
+    let file = fs::read_to_string("./data.txt").expect("Couldn't read file.");
     let lines: Vec<&str> = file.lines().collect();
 
     let problem: Problem = get_problem(lines);
@@ -58,23 +46,21 @@ fn get_problem(lines: Vec<&str>) -> Problem {
         .try_into()
         .unwrap();
 
-    let mut maps: Vec<Map> = vec![];
+    let mut locations: Vec<Location> = vec![];
 
     for i in 2..lines.len() {
         let parts: Vec<&str> = lines[i].split(" = ").collect();
-        let address: [char; 3] = parts[0].chars().collect::<Vec<char>>().try_into().unwrap();
+        let address: &str = parts[0];
 
-        let destinations: Vec<Address> = parts[1]
+        let destinations: Vec<Box<str>> = parts[1]
             .replace("(", "")
             .replace(")", "")
             .split(", ")
-            .map(|s| Address {
-                chars: s.chars().collect::<Vec<char>>().try_into().unwrap(),
-            })
+            .map(|s| Box::from(s))
             .collect();
 
-        maps.push(Map {
-            address: address.try_into().unwrap(),
+        locations.push(Location {
+            address: Box::from(address),
             destinations: HashMap::from([
                 (Direction::L, destinations[0].clone()),
                 (Direction::R, destinations[1].clone()),
@@ -82,17 +68,48 @@ fn get_problem(lines: Vec<&str>) -> Problem {
         });
     }
 
-    Problem { directions, maps }
+    Problem {
+        directions,
+        locations,
+    }
 }
 
 fn get_result(problem: Problem) -> u32 {
-    let zzz: Address = Address {
-        chars: ['Z', 'Z', 'Z'],
-    };
+    let mut found: bool = false;
 
-    let target: Address = problem.maps[0].clone().address;
-    let found: Vec<(Map, Direction)> = vec![];
+    let mut result: u32 = 0;
+    let mut location: Location = find_location(&problem, &Box::from("AAA"));
 
+    while !found {
+        for d in &problem.directions {
+            match d {
+                Direction::L => {
+                    location = find_location(&problem, &location.destinations[&Direction::L]);
+                }
+                Direction::R => {
+                    location = find_location(&problem, &location.destinations[&Direction::R]);
+                }
+            }
+            result += 1;
 
-    return 0;
+            if location.address == Box::from("ZZZ") {
+                found = true;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+fn find_location(problem: &Problem, add: &Box<str>) -> Location {
+    problem
+        .locations
+        .iter()
+        .filter(|n| n.address == *add)
+        .map(|l| l.clone())
+        .collect::<Vec<Location>>()
+        .first()
+        .unwrap()
+        .clone()
 }
