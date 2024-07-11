e12243002b62c60e364dfc5ba8df53a8969dbd97
JWATKDK <bryantjohnnyc@johndeere.com>
2024-07-11 09:42:09 -0500

    Clippy

--- day12p2/src/main.rs
+++ day12p2/src/main.rs
@@ -1,5 +1,4 @@
 use crate::Status::{Broken, Unknown, Working};
-use itertools::{Itertools};
 use std::fs;
 use memoize::memoize;
 
@@ -21,13 +20,6 @@ impl From<char> for Status {
     }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct Coord {
-    i: usize,
-    j: usize,
-    id: u32,
-}
-
 fn main() {
     let data = fs::read_to_string("./data.txt").expect("Couldn't read file.");
 
@@ -42,13 +34,13 @@ fn main() {
 }
 
 fn get_map(data: String) -> Vec<(Vec<u8>, Vec<Status>)> {
-    data.lines().map(|l| get_parts(l)).collect()
+    data.lines().map(get_parts).collect()
 }
 
 fn get_parts(l: &str) -> (Vec<u8>, Vec<Status>) {
     let parts = l.split_whitespace().collect::<Vec<&str>>();
 
-    let mut counts: Vec<u8> = parts
+    let counts: Vec<u8> = parts
     .last()
         .unwrap()
         .split(',')
@@ -62,13 +54,13 @@ fn get_parts(l: &str) -> (Vec<u8>, Vec<Status>) {
 
     let mut part2: Vec<Status> = part2_chars
         .iter()
-        .map(|c| <char as TryInto<Status>>::try_into(c.clone()).unwrap())
+        .map(|c| <char as Into<Status>>::into(*c))
         .collect();
 
-    for i in 0..4 {
+    for _i in 0..4 {
         part2.push(Unknown);
         for char in &part2_chars {
-            part2.push((*char).try_into().unwrap())
+            part2.push((*char).into())
         }
     }
 
@@ -77,13 +69,13 @@ fn get_parts(l: &str) -> (Vec<u8>, Vec<Status>) {
 
 #[memoize] // If we've seen this pattern before just return what we have in cache.
 fn get_result(groups: Vec<u8>, springs: Vec<Status>) -> u64 {
-    if groups.len() == 0 {
+    if groups.is_empty() {
         return if !springs.iter().any(|s| s == &Broken) {
             1
         } else { 0 };
     }
 
-    if springs.len() == 0 {
+    if springs.is_empty() {
         return 0
     }
 
@@ -91,29 +83,29 @@ fn get_result(groups: Vec<u8>, springs: Vec<Status>) -> u64 {
     let next_group = groups[0];
 
     if next_status == Working {
-        return working(&groups, &springs) // Next status is working and can act as a separator so we keep going
+        working(&groups, &springs) // Next status is working and can act as a separator so we keep going
     } else if next_status == Broken {
         // Ooo we found some broken springs check if they match the next group
-        return broken(&groups, &springs, next_group)
+        broken(&groups, &springs, next_group)
     } else {
         // Ooo wildcard (See if it helps us match the next group or keep going assuming the wildcard is a separator and sum the two)
-        return working(&groups, &springs) + broken(&groups, &springs, next_group);
+        working(&groups, &springs) + broken(&groups, &springs, next_group)
     }
 }
 
-fn working(groups: &Vec<u8>, springs: &Vec<Status>) -> u64 {
-    get_result(groups.clone(), springs[1..].to_vec())
+fn working(groups: &[u8], springs: &[Status]) -> u64 {
+    get_result(groups.to_owned(), springs[1..].to_vec())
 }
 
-fn broken(groups: &Vec<u8>, springs: &Vec<Status>, next_group: u8) -> u64 {
+fn broken(groups: &[u8], springs: &[Status], next_group: u8) -> u64 {
     let mut current_group: Vec<Status> = if springs.len() < next_group as usize
     {
-        springs.clone()
+        springs.to_owned()
     } else { springs[..next_group as usize].to_vec() };
 
     // Replace Unknown with Broken
     current_group = current_group.iter()
-        .map(|s| if *s == Unknown { Broken } else { s.clone() }).collect();
+        .map(|s| if *s == Unknown { Broken } else { *s }).collect();
 
     // This group isn't a match
     if current_group != vec![Broken; next_group as usize] {
@@ -129,8 +121,8 @@ fn broken(groups: &Vec<u8>, springs: &Vec<Status>, next_group: u8) -> u64 {
         }
     }
 
-    if vec![Unknown, Working].contains(&springs[next_group as usize]) {
+    if [Unknown, Working].contains(&springs[next_group as usize]) {
         // If the next item in springs after this group is a separator we can keep going
-        return get_result(groups[1..].to_vec(), springs[next_group as usize + 1..].to_vec())
+        get_result(groups[1..].to_vec(), springs[next_group as usize + 1..].to_vec())
     } else { 0 }
 }
